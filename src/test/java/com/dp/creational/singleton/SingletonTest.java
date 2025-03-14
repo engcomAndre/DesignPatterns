9d620f170cd001ed652ccd679cac8b75a889237f
andre-vieira <andre.vieira@invillia.com>
2025-03-14 10:41:10 -0300

    define order execution od tests

--- src/test/java/com/dp/creational/singleton/SingletonTest.java
+++ src/test/java/com/dp/creational/singleton/SingletonTest.java
@@ -1,6 +1,9 @@
 package com.dp.creational.singleton;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -8,22 +11,29 @@ import java.util.concurrent.CountDownLatch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SingletonTest {
 
     @Test
+    @Order(10)
     void shouldBeSameInstance() {
         BasicSingleton instance1 = BasicSingleton.getInstance();
         BasicSingleton instance2 = BasicSingleton.getInstance();
-        assert(instance1 == instance2);
+
+        BasicSingletonThreadSafe instance3 = BasicSingletonThreadSafe.getInstance();
+        BasicSingletonThreadSafe instance4 = BasicSingletonThreadSafe.getInstance();
+
+        assert (instance1 == instance2);
+        assert (instance3 == instance4);
     }
 
     /*
-    * This evaluates the unsafeness of the BasicSingleton implementation the Set instances should have more than one instance of same object
-    * but the expected result is that the Set instances should have only one instance of the same object this is fail because the BasicSingleton is not thread safe
-    * and the getInstance method is not synchronized, so the threads can instantiate more than one instance of the singleton class
-    * */
+     * This evaluates the unsafeness of the BasicSingleton implementation the Set instances should have more than one instance of same object
+     * but the expected result is that the Set instances should have only one instance of the same object this is fail because the BasicSingleton is not thread safe
+     * and the getInstance method is not synchronized, so the threads can instantiate more than one instance of the singleton class
+     * */
     @Test
+    @Order(1)
     void testUnsafenessBasicSingleton() throws InterruptedException {
         Set<BasicSingleton> instances = new HashSet<>();
         int threadCount = 1000;
@@ -56,13 +66,13 @@ class SingletonTest {
         assertThat(instances).hasSizeGreaterThan(1);
     }
 
-
     /*
      * This evaluates the safeness of the BasicSingleton implementation the Set instances should have same instance of same class
      * but this approach is thread safe, because the getInstance method is synchronized, so the threads can instantiate only one instance of the singleton class
      * this increase a performance issue, because the synchronized block is a bottleneck
      * */
     @Test
+    @Order(10)
     void testSafenessBasicSingleton() throws InterruptedException {
         Set<BasicSingletonThreadSafe> instances = new HashSet<>();
         int threadCount = 1000;
