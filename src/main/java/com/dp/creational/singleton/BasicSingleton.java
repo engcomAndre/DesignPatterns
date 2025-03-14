e0133a8db58c38f2d2c03ee0b6d4db0e0c28a724
andre-vieira <andre.vieira@invillia.com>
2025-03-14 10:41:42 -0300

    define order execution tests

--- src/main/java/com/dp/creational/singleton/BasicSingleton.java
+++ src/main/java/com/dp/creational/singleton/BasicSingleton.java
@@ -15,7 +15,7 @@ public class BasicSingleton {
     public static BasicSingleton getInstance() {
         if (instance == null) {
             try {
-                Thread.sleep(1000); // Simulating a delay to increase race condition probability
+                Thread.sleep(10000); // Simulating a delay to increase race condition probability
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
