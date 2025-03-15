a188bf58e106f90798de11a9a6cb89eae99049b7
andre-vieira <andre.vieira@invillia.com>
2025-03-15 15:02:24 -0300

    init playground of factory tests

---
+++ src/test/java/com/dp/creational/factory/FactoryTest.java
@@ -0,0 +1,38 @@
+package com.dp.creational.factory;
+
+import com.dp.creational.playground.Factory;
+import com.dp.creational.playground.FactorySomeThingFirst;
+import com.dp.creational.playground.FactorySomeThingSecond;
+import com.dp.creational.playground.ParameterizedFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FactoryTest {
+
+    @Test
+    void testFactory() {
+        final Factory factoryFirst = new FactorySomeThingFirst();
+        final Factory factorySecond = new FactorySomeThingSecond();
+
+        assertThat(factoryFirst).isNotSameAs(factorySecond);
+        assertThat(factoryFirst.doSomething()).isEqualTo("FactorySomeThingFirst doSomething");
+        assertThat(factorySecond.doSomething()).isEqualTo("FactorySomeThingSecond doSomething");
+    }
+
+    @Test
+    void testFactoryWithParameterizedFactory() {
+        final Factory factoryFirst = ParameterizedFactory.createFactory("FactorySomeThingFirst");
+        final Factory factorySecond = ParameterizedFactory.createFactory("FactorySomeThingSecond");
+
+        assertThat(factoryFirst).isNotSameAs(factorySecond);
+        assertThat(factoryFirst.doSomething()).isEqualTo("FactorySomeThingFirst doSomething");
+        assertThat(factorySecond.doSomething()).isEqualTo("FactorySomeThingSecond doSomething");
+    }
+
+    @Test
+    void testFactoryWithParameterizedFactoryNotExists() {
+        assertThat(ParameterizedFactory.createFactory("Not exists")).isNull();
+    }
+
+}
